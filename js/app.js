a1a74b46c65db2f9b67f8fa4bf223c1b331b7d35
ukpono-j <ukponoakpan270@gmail.com>
2022-05-05 16:14:18 +0100

    making changes to my navigation bar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-$("#search-icon").click(function() {
+$("#search-icon").click(function () {
     $(".nav").toggleClass("search");
     $(".nav").toggleClass("no-search");
     $(".search-input").toggleClass("search-active");
-  });
-  
-  $('.menu-toggle').click(function(){
-     $(".nav").toggleClass("mobile-nav");
-     $(this).toggleClass("is-active");
-  });
-  
+});
+
+$('.menu-toggle').click(function () {
+    $(".nav").toggleClass("mobile-nav");
+    $(this).toggleClass("is-active");
+});
+
+
+
+
