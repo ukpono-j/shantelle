c56b8914a8e79a539c38be4193c313d88a6d4a3e
ukpono-j <ukponoakpan270@gmail.com>
2022-05-05 17:20:22 +0100

    made changes to  the navigation bar of all pages

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
-$("#search-icon").click(function () {
-    $(".nav").toggleClass("search");
-    $(".nav").toggleClass("no-search");
-    $(".search-input").toggleClass("search-active");
-});
-
-$('.menu-toggle').click(function () {
-    $(".nav").toggleClass("mobile-nav");
-    $(this).toggleClass("is-active");
-});
 
 
-
-
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    //Toggle Nav
+    burger.addEventListener('click', ()=>{
+      nav.classList.toggle('nav-active');
+      
+      //Animate Links
+      navLinks.forEach((link, index)=>{
+        if(link.style.animation){
+          link.style.animation = ''
+        }else{
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+  
+        }
+      });
+      
+      //burger animation
+      burger.classList.toggle('toggle');
+      
+      
+    });
+    
+    
+    
+    
+  }
+  
+  navSlide();
